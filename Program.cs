b49ba541e63ce5d0a35e04ce070f820f6135e4da
Jarek <jaroslaw.pela@wp.pl>
2023-11-12 23:25:13 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Vehicle_Inheritance_Excercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
