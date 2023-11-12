61b65c7326aafff5fc91009c2ff06b6bd61802a0
Jarek <jaroslaw.pela@wp.pl>
2023-11-13 00:42:28 +0100

    declaration abstract method

--- Vehicle.cs
+++ Vehicle.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace Vehicle_Inheritance_Excercise
 {
-    public class Vehicle
+    public abstract class Vehicle
     {
+        // TODO: zdefiniuj wspólne części klas pochodnych
+        public int Model { get; set; }
+        //public int Sits { get; set; }
+
+        //public int Wheels { get; set; }
+
+        //public string Color { get; set; }
+
+        public abstract void Start();
+
+        public abstract void Stop();
+
     }
+
+    public class Car : Vehicle
+    {
+        // TODO: Uzupełnij implementację klasy Car, aby dziedziczyła po klasie Vehicle,
+        // posiadała implementacje metod Start i Stop, oraz dodatkowe metody związane z samochodem.
+        
+    }
+
+    public class Motorcycle : Vehicle
+    {
+        // TODO: Uzupełnij implementację klasy Motorcycle, aby dziedziczyła po klasie Vehicle,
+        // posiadała implementacje metod Start i Stop, oraz dodatkowe metody związane z motocyklem.
+    }
+
 }
